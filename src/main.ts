7c1b5c8b847dd6faca26fdec61fb37e68deb372c
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2025-07-28 16:16:47 +0300

    chore: app trust proxy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.set('trust proxy', 'loopback');
+  app.set('trust proxy', true);
   app.enableCors();
   app.use(helmet({
     crossOriginResourcePolicy: { policy: 'cross-origin' }, 
